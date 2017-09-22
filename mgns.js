906c8e22c88c79538f2fb83d62a47d472da1df06
budhiutomo17 <32137553+budhiutomo17@users.noreply.github.com>
2017-09-22 20:13:29 +0700

    Update mgns.js

--- mgns.js
+++ mgns.js
@@ -131,7 +131,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "http://www.motorganas.com/2017/05/kelebihan-dan-kekurangan-mesin-square-overstroke-overbore.html?url=" + encryptedUrl;
+            a_to_ve[i].href = "http://www.motorganas.com/p/cara-membuka-file-ziprar7z-dengan.html?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
